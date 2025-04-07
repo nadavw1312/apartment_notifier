3485ceafa9b6dd3ea70e6dc5ce71457cd129a3e6
omerpessach <omerpessach7@gmail.com>
2025-04-07 23:21:31 +0300

    your commit message

---
+++ src/services/mail_provider/config.py
@@ -0,0 +1,2 @@
+GMAIL_USERNAME = "dirot2411@gmail.com"
+GMAIL_PASSWORD = "dirotbot2025"
--- src/services/mail_provider/example_usage.py
+++ src/services/mail_provider/example_usage.py
@@ -1,18 +1,18 @@
 import asyncio
-from services.mail_provider.mail_provider_factory import MailProviderFactory, MailProviderType
+from src.services.mail_provider.managers.mail_provider_factory import MailProviderFactory, MailProviderType
 
 async def main():
     # Create provider instance
     provider = MailProviderFactory.create_provider(
         MailProviderType.GMAIL,
-        credentials_path='path/to/your/credentials.json'
+        credentials_path='credentials.json'
     )
     
     # Authenticate
     await provider.authenticate()
     
     # Start monitoring emails
-    search_query = "from:marketplace@example.com subject:new listing"
+    search_query = ""
     await provider.watch_inbox(search_query)
 
 if __name__ == "__main__":
